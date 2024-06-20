c9f3dce17fd77ba5daa5699637d9db45174179c8
subaru <main@su8ru.dev>
2024-06-21 01:14:29 +0900

    build: add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import remarkLinkCard from 'remark-link-card';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkLinkCard],
   },
+  integrations: [preact()],
 });
