c6112d22b9b2f34b54347f2c449955a1e3534c4d
subaru <main@su8ru.dev>
2024-06-24 23:56:41 +0900

    fix: resolve aria and seo issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://su8ru.dev',
   markdown: {
     remarkPlugins: [remarkLinkCard],
   },
